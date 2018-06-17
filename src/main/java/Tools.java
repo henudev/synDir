655b34dfedc8c6c0ae3c6655ff11d49e002cc9ee
qinzhengzz <qinzhengzz@inspur.com>
2018-06-17 16:49:11 +0800

    add ignore

---
+++ src/main/java/Tools.java
@@ -0,0 +1,11 @@
+
+
+/**
+ 工具类
+ **/
+public class Tools {
+    //比较文件MD5
+    public static boolean conTrastFileMD5 (Object object) {
+        return true;
+    }
+}
---
+++ src/main/java/io/StringTest.java
@@ -0,0 +1,6 @@
+package io;
+
+public class StringTest {
+    //十六进制转换十进制
+    public static void int
+}
