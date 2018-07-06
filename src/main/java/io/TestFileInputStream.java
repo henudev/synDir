1e5b8e648abc4ede7fe8ca06c3a364e41ea49abc
qinzheng (BDPL) <qinzheng@h3c.com>
2018-07-07 01:36:45 +0800

    add io test

---
+++ src/main/java/io/TestFileInputStream.java
@@ -0,0 +1,27 @@
+package io;
+
+import org.omg.CosNaming.NamingContextExtPackage.StringNameHelper;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class TestFileInputStream {
+    public static void main(String[] args) throws IOException {
+        String path = "d:\\test.txt";
+        TestFileInputStream testFileInputStream = new TestFileInputStream();
+        testFileInputStream.readFileByFileInputStream(path);
+    }
+
+    //test FileInputStream
+    public static void readFileByFileInputStream(String path) throws IOException {
+        FileInputStream fileInputStream = null;
+        fileInputStream = new FileInputStream(path);
+        int n = 1024;
+        byte buffer[] = new byte[n];
+        System.out.println("fileInputStream:::::");
+        while ((fileInputStream.read(buffer, 0, n)) != -1 && (n > 0)) {
+                System.out.println(new String(buffer));
+        }
+        fileInputStream.close();
+    }
+}
---
+++ src/main/java/io/TestWriter.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package io;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * @author q16926
+ *
+ */
+public class TestWriter {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		String path = "d:\\test.txt";
+		TestWriter testWriter = new TestWriter();
+		testWriter.writeFileByFileWriter(path);
+		testWriter.writeFileByBufferWriter(path);
+	}
+
+	// test Writer
+	public void writeFileByFileWriter(String path) throws IOException {
+		FileWriter fileWriter = new FileWriter(path);
+		fileWriter.write("this add ::::: \r\n");
+		fileWriter.flush();
+		fileWriter.close();
+	}
+
+	// test BufferWriter
+	public void writeFileByBufferWriter(String path) throws IOException {
+		File file = new File(path);
+		if (file.exists()) {
+			FileWriter fileWriter =  new FileWriter(file, true);
+			BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+			bufferedWriter.write("this bufferwriter::::\r\n");
+			bufferedWriter.flush();
+			fileWriter.close();
+			bufferedWriter.close();
+		}
+	}
+}
